64d469d2702b50f4aa8381d3d669173e1c2b599e
Lss233 <ls1102086624@outlook.com>
2015-12-27 15:08:21 +0800

    Update configs Signed-off-by: Lss233 <ls1102086624@outlook.com>

--- app/controller/SignController.php
+++ app/controller/SignController.php
@@ -9,12 +9,12 @@ class SignController extends \Phalcon\Mvc\Controller
     }
     public function veriflyAction()
     {
-    	$this->tag->setTitle('Sign In');
+    	$this->tag->setTitle('注册中。。。');
 
     	$username=$this->request->getPost('username');
     	$email=$this->request->getPost("email");
     	$token = md5($username.$username.$regtime); //创建用于激活识别码 
-	$token_exptime = time()+60*60*24;//过期时间为24小时后 
+        $token_exptime = time()+60*60*24;//过期时间为24小时后 
     	$user = new UserAccount();
     	$user->username = $username;
     	$user->email = $email;
@@ -27,11 +27,18 @@ class SignController extends \Phalcon\Mvc\Controller
     	print_r(time());
     	
     	if ($user->save() == true) {
+            $this->tag->setTitle('注册成功 | FiSkinAlcon');
+            $this->flash->success("注册成功！一封激活邮件已发往您的邮箱，请及时登录查看喵~");
 
-    	}
-    	foreach ($user->getMessages() as $message) {
-                echo $message->getMessage(), "<br/>";
+    	} else {
+            $this->tag->setTitle('新用户注册 | FiSkinAlcon');
+            $errmsg="注册失败咯,<br/>";
+            foreach ($user->getMessages() as $message) {
+                $errmsg.=getMessage()."<br/>"；
             }
-    }
+            $this->flash->error($errmsg)
+            }
+        }
+
 
 }
