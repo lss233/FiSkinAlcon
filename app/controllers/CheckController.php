c5d90675993f4682841758c32de466db436b13ed
Lss233 <ls1102086624@outlook.com>
2016-01-01 11:04:29 +0800

    Compete CheckController
    Signed-off-by: Lss233 <ls1102086624@outlook.com>

--- app/controllers/CheckController.php
+++ app/controllers/CheckController.php
@@ -6,7 +6,6 @@ class CheckController extends \Phalcon\Mvc\Controller
     {
     	if (!$this->request->isPost()) {
     		print_r('{"status":"0","message":"NO ACCESS"}');
-    		die;
     	}
     	parent::initialize();
     }
@@ -16,10 +15,19 @@ class CheckController extends \Phalcon\Mvc\Controller
     }
     public function usernameCheckAction()
     {
-
+        if(UserAccount::find("username = '".$this->request->getPost('username')."'")!=0){
+            print_r(false);
+        } else {
+            print_r(true);
+        }
     }
     public function emailCheckAction()
     {
+        if(UserAccount::find("email = '".$this->request->getPost('email')."'")!=0){
+            print_r(false);
+        } else {
+            print_r(true);
+        }
     }
 
 }
